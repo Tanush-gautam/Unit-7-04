ff9bee675d6c495572c2d43e14e0d454a33281d1
Tanushgautam <tanush.gautam@ocsbstudent.ca>
2021-02-22 16:03:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', mussuem)
+
+let age = 12 
+
+let day = 'saturday'
+function mussuem () {
+age = document.getElementById('age').value
+day = document.getElementById('day').value
+if (( day == 'tuesday' || day == 'thursday') || ( age > 12 && age < 21)) { 
+ document.getElementById('results').innerHTML='you are allowed for student pricing!'
+} else { 
+  document.getElementById('results').innerHTML='you must pay the original price'
+}
+}
